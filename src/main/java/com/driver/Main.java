c509467522d02a099d8ec9550acc6d72e4a686e0
AmeyPank <pankantiameya@gmail.com>
2023-09-13 15:10:38 +0530

    public Static added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,30 @@
 package com.driver;
 
 public class Main {
+
+
+
+
+  // Task 1: Create a class A with a method named meth
+  public static class A {
+    public String meth() {
+      return "Invoking method from class A";
+    }
+  }
+
+  // Task 2: Create a class B which extends class A
+  public static class B extends A {
+    // Task 4: Override the method meth in class B
+    @Override
+    public String meth() {
+      return "Method is overridden in Extended class B";
+    }
+  }
   public static void main(String[] args) {
     // Task 3: Create an object of class B
     B obj = new B();
     // Task 5: Call the overridden method from the object of class B
     System.out.println(obj.meth());
   }
-}
 
-// Task 1: Create a class A with a method named meth
-class A {
-  String meth() {
-    return "Invoking method from class A";
-  }
-}
-
-// Task 2: Create a class B which extends class A
-class B extends A {
-  // Task 4: Override the method meth in class B
-  @Override
-  String meth() {
-    return "Method is overridden in Extended class B";
-  }
 }
